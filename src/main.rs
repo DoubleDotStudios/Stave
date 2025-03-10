35fa2e4ec12b7fc3a32d12aa0a390972f012d8ff
DoubleDotStudios <doubledotgames@gmail.com>
2025-03-11 09:33:00 +1300

    Added user input and tab tracking.

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,87 @@ mod utils;
 
 use std::str::FromStr;
 
+use color_eyre::{eyre::Ok, Result};
 use config::Config;
-use crossterm::event::{self, Event};
+use crossterm::event::{self, Event, KeyCode, KeyEventKind, ModifierKeyCode};
 use ratatui::{
     layout::{self, Constraint, Layout},
     style::{Color, Style},
     widgets::{Block, BorderType, Borders, Tabs},
-    Frame,
+    DefaultTerminal, Frame,
 };
 use utils::render_page;
 
-fn main() {
-    let mut terminal = ratatui::init();
-    let mut page = 0;
-    loop {
-        terminal.draw(draw).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
-            break;
-        }
-    }
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let app_result = App::default().run(terminal);
     ratatui::restore();
+    app_result
 }
 
-fn draw(frame: &mut Frame) {
-    let config = Config::get();
+#[derive(Default)]
+struct App {
+    tab: usize,
+}
 
-    let pages = Tabs::new(config.general.tabs.clone())
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .border_type(BorderType::Rounded)
-                .border_style(
-                    Style::default()
-                        .fg(Color::from_str(&config.colors.border).expect("Invalid color!")),
-                ),
-        )
-        .style(
-            Style::default()
-                .fg(Color::from_str(&config.colors.inactive_text).expect("Invalid color!"))
-                .bg(Color::from_str(&config.colors.inactive_text_bg).expect("Invalid color!")),
-        )
-        .highlight_style(
-            Style::default()
-                .fg(Color::from_str(&config.colors.active_text).expect("Invalid color!"))
-                .bg(Color::from_str(&config.colors.active_text_bg).expect("Invalid color!")),
-        )
-        .divider(config.general.tab_spacer.clone())
-        .select(0);
+impl App {
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
 
-    let layout = Layout::new(
-        layout::Direction::Vertical,
-        vec![Constraint::Min(3), Constraint::Percentage(100)],
-    )
-    .spacing(0)
-    .split(frame.area());
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') => return Ok(()),
+                        KeyCode::Tab => self.tab += 1,
+                        KeyCode::BackTab => self.tab -= 1,
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
 
-    frame.render_widget(pages, layout[0]);
-    render_page(config.general.tabs[0].clone(), frame, layout[1], config);
+    fn draw(&self, frame: &mut Frame) {
+        let config = Config::get();
+
+        let pages = Tabs::new(config.general.tabs.clone())
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .border_type(BorderType::Rounded)
+                    .border_style(
+                        Style::default()
+                            .fg(Color::from_str(&config.colors.border).expect("Invalid color!")),
+                    ),
+            )
+            .style(
+                Style::default()
+                    .fg(Color::from_str(&config.colors.inactive_text).expect("Invalid color!"))
+                    .bg(Color::from_str(&config.colors.inactive_text_bg).expect("Invalid color!")),
+            )
+            .highlight_style(
+                Style::default()
+                    .fg(Color::from_str(&config.colors.active_text).expect("Invalid color!"))
+                    .bg(Color::from_str(&config.colors.active_text_bg).expect("Invalid color!")),
+            )
+            .divider(config.general.tab_spacer.clone())
+            .select(0);
+
+        let layout = Layout::new(
+            layout::Direction::Vertical,
+            vec![Constraint::Min(3), Constraint::Percentage(100)],
+        )
+        .spacing(0)
+        .split(frame.area());
+
+        frame.render_widget(pages, layout[0]);
+        render_page(
+            config.general.tabs[self.tab].clone(),
+            frame,
+            layout[1],
+            config,
+        );
+    }
 }
